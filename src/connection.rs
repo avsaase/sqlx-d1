4bdc66ea8e7483b7482192b49b702e08642dc51f
Alexander van Saase <avsaase@gmail.com>
2024-04-12 23:39:25 +0200

    Implement Connection functions

--- src/connection.rs
+++ src/connection.rs
@@ -1,4 +1,5 @@
 use futures_core::future::BoxFuture;
+use futures_util::future;
 use sqlx_core::{connection::Connection, transaction::Transaction, Error};
 
 use crate::{database::D1, options::D1ConnectOptions};
@@ -11,33 +12,31 @@ impl Connection for D1Connection {
     type Options = D1ConnectOptions;
 
     fn close(self) -> BoxFuture<'static, Result<(), Error>> {
-        todo!()
+        Box::pin(future::ok(()))
     }
 
     fn close_hard(self) -> BoxFuture<'static, Result<(), Error>> {
-        todo!()
+        Box::pin(future::ok(()))
     }
 
     fn ping(&mut self) -> BoxFuture<'_, Result<(), Error>> {
-        todo!()
+        Box::pin(future::ok(()))
     }
 
     fn begin(&mut self) -> BoxFuture<'_, Result<Transaction<'_, Self::Database>, Error>>
     where
         Self: Sized,
     {
-        todo!()
+        Transaction::begin(self)
     }
 
-    fn shrink_buffers(&mut self) {
-        todo!()
-    }
+    fn shrink_buffers(&mut self) {}
 
     fn flush(&mut self) -> BoxFuture<'_, Result<(), Error>> {
-        todo!()
+        Box::pin(future::ok(()))
     }
 
     fn should_flush(&self) -> bool {
-        todo!()
+        false
     }
 }
