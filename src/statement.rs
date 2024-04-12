b8c46ad535a8bdb44a1fbee8c5e91270c736392e
Alexander van Saase <avsaase@gmail.com>
2024-04-13 00:46:18 +0200

    Implement Statement functions

--- src/statement.rs
+++ src/statement.rs
@@ -1,29 +1,52 @@
-use std::marker::PhantomData;
+use std::{borrow::Cow, marker::PhantomData, sync::Arc};
 
-use sqlx_core::{impl_statement_query, statement::Statement, Either};
+use sqlx_core::{
+    column::ColumnIndex, ext::ustr::UStr, impl_statement_query, statement::Statement, Either,
+    Error, HashMap,
+};
 
 use crate::{arguments::D1Arguments, column::D1Column, database::D1, type_info::D1TypeInfo};
 
-pub struct D1Statement<'q>(PhantomData<&'q ()>);
+pub struct D1Statement<'q> {
+    sql: Cow<'q, str>,
+    parameters: usize,
+    columns: Arc<Vec<D1Column>>,
+    column_names: Arc<HashMap<UStr, usize>>,
+}
 
 impl<'q> Statement<'q> for D1Statement<'q> {
     type Database = D1;
 
     fn to_owned(&self) -> D1Statement<'static> {
-        todo!()
+        D1Statement {
+            sql: Cow::Owned(self.sql.to_string()),
+            parameters: self.parameters,
+            columns: Arc::clone(&self.columns),
+            column_names: Arc::clone(&self.column_names),
+        }
     }
 
     fn sql(&self) -> &str {
-        todo!()
+        &self.sql
     }
 
     fn parameters(&self) -> Option<Either<&[D1TypeInfo], usize>> {
-        todo!()
+        Some(Either::Right(self.parameters))
     }
 
     fn columns(&self) -> &[D1Column] {
-        todo!()
+        &self.columns
     }
 
     impl_statement_query!(D1Arguments);
+}
+
+impl ColumnIndex<D1Statement<'_>> for &str {
+    fn index(&self, statement: &D1Statement) -> Result<usize, Error> {
+        statement
+            .column_names
+            .get(*self)
+            .ok_or_else(|| Error::ColumnNotFound((*self).into()))
+            .map(|v| *v)
+    }
 }
