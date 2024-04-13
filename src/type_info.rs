aef561756db93af7f60c1fa0ee1411a0adc3c08f
Alexander van Saase <avsaase@gmail.com>
2024-04-13 22:55:52 +0200

    Implement TypeInfo functions

--- src/type_info.rs
+++ src/type_info.rs
@@ -3,20 +3,37 @@ use std::fmt::{self, Display, Formatter};
 use sqlx_core::type_info::TypeInfo;
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct D1TypeInfo;
+pub struct D1TypeInfo(DataType);
+
+#[derive(Debug, Clone, PartialEq)]
+enum DataType {
+    Null,
+    Real,
+    Integer,
+    Text,
+    Boolean,
+    Blob,
+}
 
 impl TypeInfo for D1TypeInfo {
     fn is_null(&self) -> bool {
-        todo!()
+        matches!(self.0, DataType::Null)
     }
 
     fn name(&self) -> &str {
-        todo!()
+        match self.0 {
+            DataType::Null => "NULL",
+            DataType::Real => "REAL",
+            DataType::Integer => "INTEGER",
+            DataType::Text => "TEXT",
+            DataType::Boolean => "BOOLEAN",
+            DataType::Blob => "BLOB",
+        }
     }
 }
 
 impl Display for D1TypeInfo {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        todo!()
+        f.pad(self.name())
     }
 }
